8fcaac02da6ae443ff8204a00ad7b46e0e9b6bae
Kaguya <rider0@163.com>
2024-06-13 18:01:36 +0800

    既然学校的项目自己不感兴趣，那就自己做!

---
+++ REVideos_Server/src/main/java/com/github/kaguya1210/revideos_server/ReVideosServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.kaguya1210.revideos_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReVideosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReVideosServerApplication.class, args);
+    }
+
+}
---
+++ REVideos_Server/src/test/java/com/github/kaguya1210/revideos_server/ReVideosServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kaguya1210.revideos_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReVideosServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
