01194a106aa7bd27169b188b05bbfb44183ad3e4
Kaguya <rider0@163.com>
2024-06-13 19:41:23 +0800

    哈哈哈哈哈，模仿了一下哔哩哔哩的登录按钮

--- REVideos_Web/src/main.js
+++ REVideos_Web/src/main.js
@@ -1,11 +1,20 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import store from './store'
+//1引入element-plus组件库
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//3引入element-plus图标库
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+//4使用element-plus图标库
 const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+//5遍历对象中的所有图表组件并且注册到app中
+for(const [key,component] of Object.entries(ElementPlusIconsVue)){
+    app.component(key,component)
+}
+//2使用element-plus组件库
+createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
--- REVideos_Web/src/views/HomeView.vue
+++ REVideos_Web/src/views/HomeView.vue
@@ -1,9 +1,46 @@
+<template>
+  <div>
+    <el-container>
+      <el-header style="background-color: red">
+        <el-row :gutter="30">
+
+          <el-col :span="12" style="background-color: aqua">
+            1
+          </el-col>
+
+          <el-col :span="12" class="user">
+            <el-button type="primary" class="loginButton" circle>
+              登录
+            </el-button>
+          </el-col>
+
+          <el-col :span="12" style="background-color: fuchsia">
+3
+          </el-col>
+        </el-row>
+      </el-header>
+
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+.user {
+  background-color: pink;
+  height: 80px;
+  text-align: right;
+  .loginButton {
+    width: 45px;
+    height: 45px;
+    margin-top: 20px;
+  }
+}
+
+</style>
